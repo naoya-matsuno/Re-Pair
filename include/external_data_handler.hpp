754b47bbefef2e3575ec30d3817173fc0125c2dc
naoya-matsuno <naoya.matsuno.r1@dc.tohoku.ac.jp>
2024-03-08 06:13:41 +0000

    Update

--- include/external_data_handler.hpp
+++ include/external_data_handler.hpp
@@ -19,7 +19,7 @@ class InputData {
         void set_values();
     
     public:
-        InputData(const std::string &input_file_path);
+        InputData(const std::string& input_file_path);
 
         // 入力ファイル名を取得
         std::string get_input_file_name() const;
@@ -37,13 +37,13 @@ class InputData {
         std::string get_input_file_path() const;
 
         // 入力ファイルのパスを変更
-        void set_input_file_path(const std::string &input_file_path);
+        void set_input_file_path(const std::string& input_file_path);
 
         // std::string型でテキスト全てを取得
         std::string get_all_text_by_string() const;
 
         // std::string型でテキスト一行を取得
-        std::string get_line_text_by_string(const std::size_t &line_num) const;
+        std::string get_line_text_by_string(const std::size_t& line_num) const;
 };
 
 // データの出力
@@ -53,7 +53,7 @@ class OutputData {
         std::string output_file_extension; // 出力ファイル拡張子
         std::string directory_path; // 出力ファイルを保存するディレクトリのパス
         std::string output_file_path; // 出力ファイルのパス
-        std::string delimiter_string; // データの区切りの設定
+        std::string delimiter; // データの区切りの設定
         std::vector<std::string> header; // ヘッダー
         std::vector<std::vector<std::string>> data; // 出力データ
 
@@ -64,14 +64,14 @@ class OutputData {
         void create_directory() const;
 
         // ヘッダーを書き出し
-        void write_header(std::ofstream &ofs) const;
+        void write_header(std::ofstream& ofs) const;
 
         // データを書き出し
-        void write_data(std::ofstream &ofs) const;
+        void write_data(std::ofstream& ofs) const;
 
         // データをstd::vector<std::string>に変換して返す
         template <typename T, typename ...Args>
-        std::vector<std::string> format_data(const T &data_content, const Args &...data_contents) {
+        std::vector<std::string> format_data(const T& data_content, const Args& ...data_contents) {
             std::vector<std::string> data;
             std::ostringstream oss;
 
@@ -84,9 +84,28 @@ class OutputData {
             return data;
         }
 
+        template <typename T, typename ...Args>
+        std::vector<std::string> format_data(const std::vector<T>& data_content, const Args& ...data_contents) {
+            std::vector<std::string> data;
+            std::ostringstream oss;
+
+            for (std::size_t i = 0; i < data_content.size(); i++) {
+                oss << data_content[i];
+                if (i != data_content.size() - 1)
+                    oss << ", ";
+            }
+            
+            data.push_back(oss.str());
+            
+            std::vector<std::string> rest_data = format_data(data_contents...);
+            data.insert(data.end(), std::make_move_iterator(rest_data.begin()), std::make_move_iterator(rest_data.end()));
+
+            return data;
+        }
+
         // データをstd::vector<std::string>に変換して返す
         template <typename T>
-        std::vector<std::string> format_data(const T &data_content) {
+        std::vector<std::string> format_data(const T& data_content) {
             std::vector<std::string> data;
             std::ostringstream oss;
 
@@ -96,48 +115,65 @@ class OutputData {
             return data;
         }
 
+        template <typename T>
+        std::vector<std::string> format_data(const std::vector<T>& data_content) {
+            std::vector<std::string> data;
+            std::ostringstream oss;
+
+            for (std::size_t i = 0; i < data_content.size(); i++) {
+                oss << data_content[i];
+
+                if (i != data_content.size() - 1)
+                    oss << ", ";
+            }
+            
+            data.push_back(oss.str());
+
+            return data;
+        }
+
     public:
-        // output_file_name: 出力ファイル名（拡張子抜き）, output_file_extension: 出力ファイルの拡張子, directory_path: 出力ファイルを保存するディレクトリのパス, delimiter_string: データの区切り文字
-        OutputData(const std::string &output_file_name, const std::string &output_file_extension, const std::string &directory_path, const std::string &delimiter_string);
+        // output_file_name: 出力ファイル名（拡張子抜き）, output_file_extension: 出力ファイルの拡張子, directory_path: 出力ファイルを保存するディレクトリのパス, delimiter: データの区切り文字
+        OutputData(const std::string& output_file_name, const std::string& output_file_extension, const std::string& directory_path, const std::string& delimiter);
 
         // ヘッダーの設定
-        void set_header(const std::vector<std::string> &header);
+        void set_header(const std::vector<std::string>& header);
 
         // ヘッダーの設定
         template <typename ...Args>
-        void set_header(const Args &...titles) {
+        void set_header(const Args& ...titles) {
             this->header.clear();
             this->header = format_data(titles...);
         }
 
         // ヘッダーの追加
-        void append_header(const std::string &title);
+        void append_header(const std::string& title);
 
         // ヘッダーをクリア
         void clear_header();
 
         // 出力データの設定
-        void set_data(const std::vector<std::string> &data_content);
+        void set_data(const std::vector<std::string>& data_content);
         
         // 出力データの設定
-        void set_data(const std::vector<std::vector<std::string>> &data_contents);
+        void set_data(const std::vector<std::vector<std::string>>& data_contents);
         
         // 出力データの設定
         template <typename ...Args>
-        void set_data(const Args &...data_contents) {
+        void set_data(const Args& ...data_contents) {
             this->data.clear();
             this->data.push_back(format_data(data_contents...));
         }
 
         // 出力データの追加
-        void append_data(const std::vector<std::string> &data_content);
+        void append_data(const std::vector<std::string>& data_content);
 
         // 出力データの追加
-        void append_data(const std::vector<std::vector<std::string>> &data_contents);
+        void append_data(const std::vector<std::vector<std::string>>& data_contents);
 
         // 出力データの追加
         template <typename ...Args>
-        void append_data(const Args &...data_contents) {
+        void append_data(const Args& ...data_contents) {
             this->data.push_back(format_data(data_contents...));
         }
 
@@ -145,34 +181,34 @@ class OutputData {
         void clear_data();
 
         // output_file_nameを変更
-        void set_output_file_name(const std::string &output_file_name);
+        void set_output_file_name(const std::string& output_file_name);
         
         // output_file_extensionを変更
-        void set_output_file_extension(const std::string &output_file_extension);
+        void set_output_file_extension(const std::string& output_file_extension);
 
         // directory_pahtを変更
-        void set_directory_path(const std::string &set_directory_path);
+        void set_directory_path(const std::string& set_directory_path);
 
         // delimiter_stringを変更
-        void set_delimiter_string(const std::string &delimiter_string);
+        void set_delimiter_string(const std::string& delimiter);
 
         // std::ios::trunc 上書き, std::ios::app 末尾に追加 
-        void output(const std::ios_base::openmode &mode, const bool &is_header) const;
+        void output(const std::ios_base::openmode& mode=std::ios::trunc, const bool& is_header=false) const;
 };
 
 // 複数のOutputDataオブジェクトを作成
-std::vector<OutputData> create_OutputData_objects(const std::vector<std::string> &output_file_names, const std::string &output_file_extension, const std::string &output_directory_path, const std::string &delimiter);
+std::vector<OutputData> create_OutputData_objects(const std::vector<std::string>& output_file_names, const std::string& output_file_extension, const std::string& output_directory_path, const std::string& delimiter);
 
 // ディレクトリ内のすべてのファイルパスを取得
-std::vector<std::string> get_all_file_paths_in_directory(const std::string &directory_path);
+std::vector<std::string> get_all_file_paths_in_directory(const std::string& directory_path);
 
 // ディレクトリ下のすべてのファイルパスを取得
-std::vector<std::string> get_all_file_paths_under_directory(const std::string &directory_path);
+std::vector<std::string> get_all_file_paths_under_directory(const std::string& directory_path);
 
 // ディレクトリ内の特定の拡張子のファイルパスを取得
-std::vector<std::string> get_any_extension_file_paths_in_directory(const std::string &directory_path, const std::string &extension);
+std::vector<std::string> get_any_extension_file_paths_in_directory(const std::string& directory_path, const std::string& extension);
 
 // ディレクトリ下の特定の拡張子のファイルパスを取得
-std::vector<std::string> get_any_extension_file_paths_under_directory(const std::string &directory_path, const std::string &extension);
+std::vector<std::string> get_any_extension_file_paths_under_directory(const std::string& directory_path, const std::string& extension);
 
 #endif
--- src/external_data_handler.cpp
+++ src/external_data_handler.cpp
@@ -27,7 +27,7 @@ void InputData::set_values() {
         input_file_extension = input_file_path.substr(extension_pos + 1, input_file_path.size() - extension_pos - 1);
 }
 
-InputData::InputData(const std::string &input_file_path) {
+InputData::InputData(const std::string& input_file_path) {
     this->input_file_path = input_file_path;
     set_values();
 }
@@ -61,7 +61,7 @@ std::string InputData::get_input_file_path() const {
 }
 
 // 入力ファイルのパスを変更
-void InputData::set_input_file_path(const std::string &input_file_path) {
+void InputData::set_input_file_path(const std::string& input_file_path) {
     this->input_file_path = input_file_path;
     set_values();
 }
@@ -84,7 +84,7 @@ std::string InputData::get_all_text_by_string() const {
 }
 
 // std::string型でテキスト一行を取得
-std::string InputData::get_line_text_by_string(const std::size_t &line_num) const {
+std::string InputData::get_line_text_by_string(const std::size_t& line_num) const {
     std::string line;
     std::fstream ifs;
     
@@ -130,7 +130,7 @@ void OutputData::create_directory() const {
 }
 
 // ヘッダーを書き出し
-void OutputData::write_header(std::ofstream &ofs) const {
+void OutputData::write_header(std::ofstream& ofs) const {
     for (std::size_t i = 0; i < header.size(); i++) {
         ofs << header[i];
         if (i < header.size() - 1)
@@ -141,13 +141,13 @@ void OutputData::write_header(std::ofstream &ofs) const {
 }
 
 // データを書き出し
-void OutputData::write_data(std::ofstream &ofs) const {
+void OutputData::write_data(std::ofstream& ofs) const {
     if (ofs) {
-        for (const std::vector<std::string> &data_content : data) {
+        for (const std::vector<std::string>& data_content : data) {
             for (std::size_t i = 0; i < data_content.size(); i++) {
                 ofs << data_content[i];
                 if (i < data_content.size() - 1)
-                    ofs << delimiter_string;
+                    ofs << delimiter;
                 else
                     ofs << std::endl;
             }
@@ -155,23 +155,23 @@ void OutputData::write_data(std::ofstream &ofs) const {
     }
 }
 
-// output_file_name: 出力ファイル名（拡張子抜き）, output_file_extension: 出力ファイルの拡張子, directory_path: 出力ファイルを保存するディレクトリのパス, delimiter_string: データの区切り文字
-OutputData::OutputData(const std::string &output_file_name, const std::string &output_file_extension, const std::string &directory_path, const std::string &delimiter_string) {
+// output_file_name: 出力ファイル名（拡張子抜き）, output_file_extension: 出力ファイルの拡張子, directory_path: 出力ファイルを保存するディレクトリのパス, delimiter: データの区切り文字
+OutputData::OutputData(const std::string& output_file_name, const std::string& output_file_extension, const std::string& directory_path, const std::string& delimiter) {
     this->output_file_name = output_file_name;
     this->output_file_extension = output_file_extension;
     this->directory_path = directory_path;
-    this->delimiter_string = delimiter_string;
+    this->delimiter = delimiter;
     update_output_file_path();
 }
 
 // ヘッダーの設定
-void OutputData::set_header(const std::vector<std::string> &header) {
+void OutputData::set_header(const std::vector<std::string>& header) {
     clear_header();
     this->header = header;
 }
 
 // ヘッダーの追加
-void OutputData::append_header(const std::string &title) {
+void OutputData::append_header(const std::string& title) {
     this->header.push_back(title);
 }
 
@@ -181,25 +181,25 @@ void OutputData::clear_header() {
 }
 
 // 出力データの設定
-void OutputData::set_data(const std::vector<std::string> &data_content) {
+void OutputData::set_data(const std::vector<std::string>& data_content) {
     clear_data();
     this->data.push_back(data_content);
 }
 
 // 出力データの設定
-void OutputData::set_data(const std::vector<std::vector<std::string>> &data_contents) {
+void OutputData::set_data(const std::vector<std::vector<std::string>>& data_contents) {
     clear_data();
     this->data = data_contents;
 }
 
 // 出力データの追加
-void OutputData::append_data(const std::vector<std::string> &data_content) {
+void OutputData::append_data(const std::vector<std::string>& data_content) {
     this->data.push_back(data_content);
 }
 
 // 出力データの追加
-void OutputData::append_data(const std::vector<std::vector<std::string>> &data_contents) {
-    for (const std::vector<std::string> &data_content : data_contents)
+void OutputData::append_data(const std::vector<std::vector<std::string>>& data_contents) {
+    for (const std::vector<std::string>& data_content : data_contents)
         this->data.push_back(data_content);
 }
 
@@ -209,30 +209,30 @@ void OutputData::clear_data() {
 }
 
 // output_file_nameを変更
-void OutputData::set_output_file_name(const std::string &output_file_name) {
+void OutputData::set_output_file_name(const std::string& output_file_name) {
     this->output_file_name = output_file_name;
     update_output_file_path();
 }
 
 // output_file_extensionを変更
-void OutputData::set_output_file_extension(const std::string &output_file_extension) {
+void OutputData::set_output_file_extension(const std::string& output_file_extension) {
     this->output_file_extension = output_file_extension;
     update_output_file_path();
 }
 
 // directory_pahtを変更
-void OutputData::set_directory_path(const std::string &set_directory_path) {
+void OutputData::set_directory_path(const std::string& set_directory_path) {
     this->directory_path = directory_path;
     update_output_file_path();
 }
 
 // delimiter_stringを変更
-void OutputData::set_delimiter_string(const std::string &delimiter_string) {
-    this->delimiter_string = delimiter_string;
+void OutputData::set_delimiter_string(const std::string& delimiter) {
+    this->delimiter = delimiter;
 }
 
 // std::ios::trunc 上書き, std::ios::app 末尾に追加 
-void OutputData::output(const std::ios_base::openmode &mode, const bool &is_header) const {
+void OutputData::output(const std::ios_base::openmode& mode, const bool& is_header) const {
     create_directory();
 
     std::ofstream ofs;
@@ -254,10 +254,10 @@ void OutputData::output(const std::ios_base::openmode &mode, const bool &is_head
 
 
 // 複数のOutputDataオブジェクトを作成
-std::vector<OutputData> create_OutputData_objects(const std::vector<std::string> &output_file_names, const std::string &output_file_extension, const std::string &output_directory_path, const std::string &delimiter) {
+std::vector<OutputData> create_OutputData_objects(const std::vector<std::string>& output_file_names, const std::string& output_file_extension, const std::string& output_directory_path, const std::string& delimiter) {
     std::vector<OutputData> output_data_objects;
 
-    for (const std::string &output_file_name : output_file_names) {
+    for (const std::string& output_file_name : output_file_names) {
         OutputData output_data_object(output_file_name, output_file_extension, output_directory_path, delimiter);
         output_data_objects.push_back(output_data_object);
     }
@@ -266,9 +266,9 @@ std::vector<OutputData> create_OutputData_objects(const std::vector<std::string>
 }
 
 // ディレクトリ内のすべてのファイルパスを取得
-std::vector<std::string> get_all_file_paths_in_directory(const std::string &directory_path) {
+std::vector<std::string> get_all_file_paths_in_directory(const std::string& directory_path) {
     std::vector<std::string> file_paths;
-    for (const auto &file : std::filesystem::directory_iterator(directory_path)) {
+    for (const auto& file : std::filesystem::directory_iterator(directory_path)) {
         if (!file.is_directory())
             file_paths.push_back(file.path().string());
     }
@@ -277,13 +277,13 @@ std::vector<std::string> get_all_file_paths_in_directory(const std::string &dire
 }
 
 // ディレクトリ下のすべてのファイルパスを取得
-std::vector<std::string> get_all_file_paths_under_directory(const std::string &directory_path) {
+std::vector<std::string> get_all_file_paths_under_directory(const std::string& directory_path) {
     std::vector<std::string> file_paths;
-    for (const auto &file : std::filesystem::directory_iterator(directory_path)) {
+    for (const auto& file : std::filesystem::directory_iterator(directory_path)) {
         if (!file.is_directory())
             file_paths.push_back(file.path().string());
         else
-            for (const std::string &file_path : get_all_file_paths_under_directory(file.path().string()))
+            for (const std::string& file_path : get_all_file_paths_under_directory(file.path().string()))
                 file_paths.push_back(file_path);
     }
     
@@ -291,9 +291,9 @@ std::vector<std::string> get_all_file_paths_under_directory(const std::string &d
 }
 
 // ディレクトリ内の特定の拡張子のファイルパスを取得
-std::vector<std::string> get_any_extension_file_paths_in_directory(const std::string &directory_path, const std::string &extension) {
+std::vector<std::string> get_any_extension_file_paths_in_directory(const std::string& directory_path, const std::string& extension) {
     std::vector<std::string> file_paths;
-    for (const auto &file : std::filesystem::directory_iterator(directory_path)) {
+    for (const auto& file : std::filesystem::directory_iterator(directory_path)) {
         std::string file_path = file.path().string();
         std::size_t extension_pos = file_path.find_last_of(".");
 
@@ -313,9 +313,9 @@ std::vector<std::string> get_any_extension_file_paths_in_directory(const std::st
 }
 
 // ディレクトリ下の特定の拡張子のファイルパスを取得
-std::vector<std::string> get_any_extension_file_paths_under_directory(const std::string &directory_path, const std::string &extension) {
+std::vector<std::string> get_any_extension_file_paths_under_directory(const std::string& directory_path, const std::string& extension) {
     std::vector<std::string> file_paths;
-    for (const auto &file : std::filesystem::directory_iterator(directory_path)) {
+    for (const auto& file : std::filesystem::directory_iterator(directory_path)) {
         if (!file.is_directory()) {
             std::string file_path = file.path().string();
             std::size_t extension_pos = file_path.find_last_of(".");
@@ -330,7 +330,7 @@ std::vector<std::string> get_any_extension_file_paths_under_directory(const std:
             }
         }
         else {
-            for (const std::string &file_path : get_any_extension_file_paths_under_directory(file.path().string(), extension))
+            for (const std::string& file_path : get_any_extension_file_paths_under_directory(file.path().string(), extension))
                 file_paths.push_back(file_path);
         }
     }
